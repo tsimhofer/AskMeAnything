8b3024a4632abb861cd962aab37f071ba0537163
Thomas <thomas.simhofer@bestheads.com>
2021-09-21 20:20:20 +0200

    init

---
+++ AskMeAnything/AskMeAnything/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AskMeAnything
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
